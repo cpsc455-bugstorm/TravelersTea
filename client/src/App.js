e76efd6d4e91ce059cd9a1dcd2fbb2301975dd26
Lymeng Naret <lymengnaret@yahoo.com>
2023-05-22 22:25:23 -0700

    add and apply eslint and prettier to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import logo from './logo.svg'
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
