90b686b6340696cc045386bc703c8e536ae64010
Rithin Kumar <36722714+L0Lmaker@users.noreply.github.com>
2023-05-23 17:48:50 -0700

    Revert "Initialize frontend (#11)"
    This reverts commit c3abc853d4bc3886a13245d9e0891bd1a9fc7451.

--- client/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import sidebarReducer from './reducers/sidebarSlice';
-
-export const store = configureStore({
-    reducer: {
-        sidebar: sidebarReducer,
-    },
-})
