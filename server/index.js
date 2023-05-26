605c5d7482384b8e49754cdd0fd71f580a26ab93
AndyLiang1 <andyliang1000@gmail.com>
2023-05-26 16:16:20 -0700

    Routes -> Controllers -> Model fo r User Entity and Mongo DB set up

--- server/index.js
+++ server/index.js
@@ -1,11 +1,34 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+const UserRoute = require('./routes/UserRoute')
+const config = require('./config/config')
 const app = express()
-const port = 5000
+app.use(express.json())
+
+const apiRouter = express.Router()
+app.use('/api', apiRouter)
+
+const userRoute = new UserRoute()
+userRoute.initRoutes(apiRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+const connectDB = async () => {
+  try {
+    await mongoose.connect(config.mongo.uri)
+    startServer()
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const startServer = () => {
+  app.listen(config.server.port, () => {
+    console.log(`Server is running on port ${config.server.port}`)
+  })
+}
+
+connectDB()
