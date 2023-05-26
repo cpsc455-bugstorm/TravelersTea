597290cc7d5195330a8018001cf04fa4dcb613a1
NLmeng <NLmeng@users.noreply.github.com>
2023-05-26 19:22:26 +0000

    Format code

--- client/src/components/common/Button.js
+++ client/src/components/common/Button.js
@@ -19,4 +19,3 @@ export function Button({ text, onClick, className = '' }) {
     </button>
   )
 }
-;
--- server/index.js
+++ server/index.js
@@ -9,4 +9,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
-;
