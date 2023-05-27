3e29946784c1a1530a088be81227c82e26cd247b
dashdot <30674558+Graywing13@users.noreply.github.com>
2023-05-26 23:45:30 -0700

    Make pin less scuffed

--- client/src/components/common/Pin.js
+++ client/src/components/common/Pin.js
@@ -12,18 +12,24 @@ Pin.propTypes = {
 }
 
 export function Pin({ emoji, color, titleText, bodyText, className }) {
+  const showPin = color !== undefined
+
   return (
     <div className={className}>
       <div
         className={`relative h-32 w-32 cursor-pointer transition hover:drop-shadow-[0_10px_6px_rgba(0,0,0,0.25)]`}
       >
-        {color && (
+        {showPin && (
           <RoomIcon
             className="absolute -bottom-3 left-0"
             sx={{ color, fontSize: PIN_WIDTH_PX }}
           />
         )}
-        <span className="absolute left-0 top-8 w-full cursor-pointer text-center text-5xl">
+        <span
+          className={`absolute top-8 w-full cursor-pointer text-center text-5xl ${
+            showPin ? 'left-0' : '-left-16'
+          }`}
+        >
           {emoji}
         </span>
       </div>
