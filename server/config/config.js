81f3701cbfdee9619e458933a22240bb02b0bd53
Lymeng Naret <lymengnaret@yahoo.com>
2023-05-26 23:32:04 -0700

    use port 5001 instead of 5000: https://developer.apple.com/forums/thread/682332

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const MONGO_USERNAME = process.env.MONGO_USERNAME || ''
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || ''
 const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.4bflh41.mongodb.net/BugStormDB?retryWrites=true&w=majority`
-const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 5000
+const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 5001
 const ENV = process.env.ENV || ''
 const JWT_SECRET = process.env.JWT_SECRET || ''
 
