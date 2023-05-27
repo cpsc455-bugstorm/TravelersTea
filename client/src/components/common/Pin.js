2496e849b4ce57cb8c2eab71d72e3419d599bca9
dashdot <30674558+Graywing13@users.noreply.github.com>
2023-05-26 23:41:19 -0700

    Make scuffed vertical version

--- client/src/components/common/Pin.js
+++ client/src/components/common/Pin.js
@@ -4,28 +4,31 @@ import PropTypes from 'prop-types'
 const PIN_WIDTH_PX = 128
 
 Pin.propTypes = {
-  color: PropTypes.string.isRequired,
   emoji: PropTypes.string.isRequired,
+  color: PropTypes.string,
   titleText: PropTypes.string,
   bodyText: PropTypes.string,
+  className: PropTypes.string,
 }
 
-export function Pin({ color, emoji, titleText, bodyText }) {
+export function Pin({ emoji, color, titleText, bodyText, className }) {
   return (
-    <div>
-      <p className="text-lg font-bold">{titleText}</p>
-      <p className="w-64 text-base font-normal">{bodyText}</p>
+    <div className={className}>
       <div
         className={`relative h-32 w-32 cursor-pointer transition hover:drop-shadow-[0_10px_6px_rgba(0,0,0,0.25)]`}
       >
-        <RoomIcon
-          className="absolute -bottom-3 left-0"
-          sx={{ color, fontSize: PIN_WIDTH_PX }}
-        />
+        {color && (
+          <RoomIcon
+            className="absolute -bottom-3 left-0"
+            sx={{ color, fontSize: PIN_WIDTH_PX }}
+          />
+        )}
         <span className="absolute left-0 top-8 w-full cursor-pointer text-center text-5xl">
           {emoji}
         </span>
       </div>
+      <p className="text-lg font-bold text-slate-50">{titleText}</p>
+      <p className="w-64 text-base font-normal text-slate-50">{bodyText}</p>
     </div>
   )
 }
