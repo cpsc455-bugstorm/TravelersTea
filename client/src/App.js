f34a47db5d88baab62f86729fbd0a31f7e69e38c
vaishnavi <vaishnavisinha24@gmail.com>
2023-06-01 18:12:52 -0700

    Updated App.js with Login Screen

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,13 @@ import {
 import { AppView } from './constants/enums'
 import { MapElement } from './components/MapElement'
 import { useMemo } from 'react'
-
+import LoginScreen from './components/user/LoginScreen'
+import { selectUser } from './redux/reducers/userSlice'
 function App() {
   const isSidebarOpen = useSelector((state) => state.view.isSidebarOpen)
   const appView = useSelector((state) => state.view.appView)
   const dispatch = useDispatch()
+  const user = useSelector(selectUser)
 
   const detailsContent = useMemo(() => {
     if (appView === AppView.TRIP_OVERVIEW) {
@@ -59,45 +61,50 @@ function App() {
   }, [appView])
 
   return (
-    <div className='relative h-screen w-screen overflow-hidden'>
-      <MapElement
-        className={`relative left-10 w-[calc(100%-2.5rem)] transition-all ${
-          appView === AppView.NEW_TRIP ? 'h-full' : 'h-1/2'
-        }`}
-      />
-      <div
-        className={`relative left-10 flex w-[calc(100%-2.5rem)] flex-col justify-center ${
-          appView === AppView.NEW_TRIP ? 'h-0' : 'h-1/2'
-        }`}
-      >
-        {detailsContent}
-      </div>
-      <div
-        className={`fixed left-0 top-0 z-10 flex h-full w-1/5 flex-row overflow-hidden transition-all
-                ${isSidebarOpen ? 'left-0' : 'left-[-20vw]'}`}
-      >
-        <SideBar />
-      </div>
-      <span
-        className={`fixed top-0 z-10 flex h-full items-center rounded-none transition-all ${
-          isSidebarOpen ? 'left-[20vw]' : 'left-0'
-        }`}
-      >
-        <Button
-          onClick={() => dispatch(toggleSidebar())}
-          className='h-full w-10 rounded-none bg-slate-300 text-6xl hover:bg-slate-300'
-        >
-          {isSidebarOpen ? '‹' : '›'}
-        </Button>
-      </span>
-      {isSidebarOpen && (
-        <div
-          className={`fixed left-0 top-0 z-[5] h-full w-full bg-slate-800 opacity-30`}
-          onClick={() => dispatch(closeSidebar())}
-        />
+    <div>
+      {user ? ( // If user is logged in, show the app
+        <div className='relative h-screen w-screen overflow-hidden'>
+          <MapElement
+            className={`relative left-10 w-[calc(100%-2.5rem)] transition-all ${
+              appView === AppView.NEW_TRIP ? 'h-full' : 'h-1/2'
+            }`}
+          />
+          <div
+            className={`relative left-10 flex w-[calc(100%-2.5rem)] flex-col justify-center ${
+              appView === AppView.NEW_TRIP ? 'h-0' : 'h-1/2'
+            }`}
+          >
+            {detailsContent}
+          </div>
+          <div
+            className={`fixed left-0 top-0 z-10 flex h-full w-1/5 flex-row overflow-hidden transition-all
+                       ${isSidebarOpen ? 'left-0' : 'left-[-20vw]'}`}
+          >
+            <SideBar />
+          </div>
+          <span
+            className={`fixed top-0 z-10 flex h-full items-center rounded-none transition-all ${
+              isSidebarOpen ? 'left-[20vw]' : 'left-0'
+            }`}
+          >
+            <Button
+              onClick={() => dispatch(toggleSidebar())}
+              className='h-full w-10 rounded-none bg-slate-300 text-6xl hover:bg-slate-300'
+            >
+              {isSidebarOpen ? '‹' : '›'}
+            </Button>
+          </span>
+          {isSidebarOpen && (
+            <div
+              className={`fixed left-0 top-0 z-[5] h-full w-full bg-slate-800 opacity-30`}
+              onClick={() => dispatch(closeSidebar())}
+            />
+          )}
+        </div>
+      ) : (
+        <LoginScreen /> // If user not logged in, display Login screen
       )}
     </div>
   )
 }
-
 export default App
