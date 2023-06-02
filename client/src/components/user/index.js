20136c8f3cab65f2cf78c44b7723d312afe9bc55
Vaishnavi Sinha <59987667+vee-16@users.noreply.github.com>
2023-06-01 21:30:48 -0700

    Vaishnavi/login-screen (#59)
    * Added login screen and logout button
    
    * Updated App.js with Login Screen
    
    * Updated userSlice and login logout buttons
    
    * Refactored App.js content

---
+++ client/src/components/user/index.js
@@ -0,0 +1,2 @@
+export { LoginScreen } from './LoginScreen'
+export { Logout } from './Logout'
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import viewReducer from './reducers/viewSlice'
+import userReducer from './reducers/userSlice'
 
 export const store = configureStore({
   reducer: {
     view: viewReducer,
+    user: userReducer,
   },
 })
