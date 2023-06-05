01aee3d537dad1062a088108ccaae57ad44c179b
dashdot <30674558+Graywing13@users.noreply.github.com>
2023-06-04 19:02:22 -0700

    opt for 70% bg opacity on coloured cards

--- client/src/components/Timeline/TripViewContent.js
+++ client/src/components/Timeline/TripViewContent.js
@@ -44,16 +44,19 @@ export function TripViewContent() {
       return (
         <div
           key={`details-${dayNumber}`}
-          className={`from-${colorName}-400 to-${colorName}-500 my-4 box-border w-full rounded-md bg-gradient-to-br p-4 shadow-xl`}
+          className={`from-${colorName}-400/70 to-${colorName}-500/70 my-4 box-border w-full rounded-md bg-gradient-to-br p-4 shadow-xl`}
         >
-          <h3 className='text-lg font-bold'>Day {dayNumber}</h3>
+          <h3 className='text-lg font-bold text-white'>Day {dayNumber}</h3>
           {item['stages'].map((stage, stageIndex) => {
             return (
-              <div key={`details-${dayNumber}-${stageIndex}`} className='p-2'>
-                <p className='font-semibold'>
+              <div
+                key={`details-${dayNumber}-${stageIndex}`}
+                className='p-2 text-white'
+              >
+                <p className='font-semibold text-white'>
                   {stage['stageName']}: {stage['locationName']}
                 </p>
-                <p>⤷ {stage['description']}</p>
+                <p className={' text-white'}>⤷ {stage['description']}</p>
               </div>
             )
           })}
@@ -88,7 +91,7 @@ export function TripViewContent() {
   const renderDetails = useMemo(() => {
     return (
       <div
-        className={`pointer-events-auto w-full overflow-y-auto bg-slate-100 
+        className={`pointer-events-auto w-full overflow-y-auto bg-slate-900 
           ${isContentFullscreen ? 'h-1/2 p-4' : 'h-0'}`}
       >
         {cardContent}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  safelist: [{ pattern: /from-(.*)-(400)/ }, { pattern: /to-(.*)-(500)/ }],
+  safelist: [
+    { pattern: /from-(.*)-(400)\/70/ },
+    { pattern: /to-(.*)-(500)\/70/ },
+  ],
   theme: {
     extend: {},
   },
