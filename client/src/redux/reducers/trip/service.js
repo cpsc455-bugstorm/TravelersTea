b9c237c8aad26c83064c0b0d573d321c9a3c166e
Lymeng Naret <lymengnaret@yahoo.com>
2023-06-20 09:11:29 -0700

    use env for api url

--- client/src/redux/reducers/trip/service.js
+++ client/src/redux/reducers/trip/service.js
@@ -1,19 +1,20 @@
 import axios from 'axios'
+import { API_URL } from '../../../constants/API_URL'
 
 export const fetchTrips = async () => {
-  return await axios.get('http://localhost:5001/api/trip')
+  return await axios.get(`${API_URL}/trip`)
 }
 
 export const createTrip = async (tripData) => {
-  return await axios.post('http://localhost:5001/api/trip', tripData)
+  return await axios.post(`${API_URL}/trip`, tripData)
 }
 
 export const editTrip = async (id, tripData) => {
-  return await axios.patch(`http://localhost:5001/api/trip/${id}`, tripData)
+  return await axios.patch(`${API_URL}/trip/${id}`, tripData)
 }
 
 export const deleteTrip = async (id) => {
-  return await axios.delete(`http://localhost:5001/api/trip/${id}`)
+  return await axios.delete(`${API_URL}/trip/${id}`)
 }
 
 const tripService = {
