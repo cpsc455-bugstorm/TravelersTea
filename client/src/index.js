e94b69e12f982f2805cb0329a76ac9287f722071
Vaishnavi Sinha <59987667+vee-16@users.noreply.github.com>
2023-06-23 22:51:27 -0700

    Added routing for login screen and home page (#106)
    * Added routing for login and home page
    
    * Updated dispatch to pass login action as object
    
    * Wrapped App with browser router

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { store } from './redux/store'
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
 )
