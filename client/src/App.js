6ab346a88c9d679bee13cf16de2da8cb51bc8c8e
Vaishnavi Sinha <59987667+vee-16@users.noreply.github.com>
2023-06-23 18:19:31 -0700

    Wrapped App with browser router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
 import { useSelector } from 'react-redux'
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import { LoginScreen } from './components/user'
 import { AppInterface } from './AppInterface'
 import { selectUser } from './redux/reducers/userSlice'
@@ -13,18 +8,16 @@ function App() {
   const user = useSelector(selectUser)
 
   return (
-    <Router>
-      <Routes>
-        <Route
-          path='/'
-          element={user ? <Navigate to='/home' /> : <LoginScreen />}
-        />
-        <Route
-          path='/home'
-          element={user ? <AppInterface /> : <Navigate to='/' />}
-        />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route
+        path='/'
+        element={user ? <Navigate to='/home' /> : <LoginScreen />}
+      />
+      <Route
+        path='/home'
+        element={user ? <AppInterface /> : <Navigate to='/' />}
+      />
+    </Routes>
   )
 }
 
