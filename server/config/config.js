0d6adbba112d03c795827f63177879cd647b6c5e
Vaishnavi Sinha <59987667+vee-16@users.noreply.github.com>
2023-06-28 23:48:57 -0700

    Updated config to use db choice from env

--- server/config/config.js
+++ server/config/config.js
@@ -2,18 +2,20 @@ require('dotenv').config()
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || ''
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || ''
-let DB_CHOICE = 'BugStormDB'
+const DB_CHOICE = process.env.DB_USER_FLAG || 'BugStormDB'
 
-// Change the value of the DB_USER_FLAG if you want to use your own DB
-let DB_USER_FLAG = ''
-const DB_MAP = {
-  ANDY: 'BugStormDEV_AL',
-  NOREEN: 'BugStormDEV_NC',
-  MENG: 'BugStormDEV_Meng',
-  RITHIN: 'BugStormDEV_Rithin',
-  VEE: 'BugStormDEV_Vee',
-}
-DB_CHOICE = DB_MAP[DB_USER_FLAG] || 'BugStormDB'
+// let DB_CHOICE = 'BugStormDB'
+//
+// // Change the value of the DB_USER_FLAG if you want to use your own DB
+// let DB_USER_FLAG = ''
+// const DB_MAP = {
+//   ANDY: 'BugStormDEV_AL',
+//   NOREEN: 'BugStormDEV_NC',
+//   MENG: 'BugStormDEV_Meng',
+//   RITHIN: 'BugStormDEV_Rithin',
+//   VEE: 'BugStormDEV_Vee',
+// }
+// DB_CHOICE = DB_MAP[DB_USER_FLAG] || 'BugStormDB'
 
 const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.4bflh41.mongodb.net/${DB_CHOICE}?retryWrites=true&w=majority`
 const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 5001
@@ -30,5 +32,6 @@ const config = {
     JWT_SECRET,
   },
 }
+console.log('DB_CHOICE:', DB_CHOICE)
 
 module.exports = config
