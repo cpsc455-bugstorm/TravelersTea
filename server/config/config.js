2aa790db7a190b4e6853c701f229f61aaba01a1b
Vaishnavi Sinha <59987667+vee-16@users.noreply.github.com>
2023-06-29 02:46:52 -0700

    Clean up comments

--- server/config/config.js
+++ server/config/config.js
@@ -4,19 +4,6 @@ const MONGO_USERNAME = process.env.MONGO_USERNAME || ''
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || ''
 const DB_CHOICE = process.env.DB_USER_FLAG || 'BugStormDB'
 
-// let DB_CHOICE = 'BugStormDB'
-//
-// // Change the value of the DB_USER_FLAG if you want to use your own DB
-// let DB_USER_FLAG = ''
-// const DB_MAP = {
-//   ANDY: 'BugStormDEV_AL',
-//   NOREEN: 'BugStormDEV_NC',
-//   MENG: 'BugStormDEV_Meng',
-//   RITHIN: 'BugStormDEV_Rithin',
-//   VEE: 'BugStormDEV_Vee',
-// }
-// DB_CHOICE = DB_MAP[DB_USER_FLAG] || 'BugStormDB'
-
 const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.4bflh41.mongodb.net/${DB_CHOICE}?retryWrites=true&w=majority`
 const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 5001
 const ENV = process.env.ENV || ''
