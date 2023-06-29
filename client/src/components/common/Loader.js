8bb5b3d42659aaa81092dea7ff60a9a7a691a1b7
Lymeng Naret <lymengnaret@yahoo.com>
2023-06-29 16:54:47 -0700

    add some facts and finish up design for loader

--- client/src/components/common/Loader.js
+++ client/src/components/common/Loader.js
@@ -1,7 +1,31 @@
+import { TypeAnimation } from 'react-type-animation'
+
+// https://www.buzzfeed.com/ailbhemalone/15-weirdly-interesting-facts-about-tea
+// https://www.irely.com/insights/ten-fun-facts-about-tea/
+
+const funFacts = [
+  'Tea is the second most consumed drink in the world, surpassed only by water.',
+  'An estimated 3 billion cups of tea are consumed every day worldwide.',
+  'Tea was discovered by the Chinese emperor Shen Nong around 2737 BC when a tea leaf fell into his boiling water.',
+  'In 1773, American colonists dumped 342 chests of tea into the Boston Harbor to protest the Tea Act. This event is now known as the Boston Tea Party.',
+  'Tea bags were invented in the early 20th century by accident, when tea samples were sent in silk bags and recipients mistakenly steeped the whole bag.',
+  'Contrary to popular belief, it’s not just green tea that’s good for you. Black, white, and red tea also have health-giving flavonoids and polyphenols.',
+  'The very best tea comes from high elevations and is hand-picked.',
+  'In 1908, tea bags were developed by accident in New York as restuarants began brewing merchant’s tea directly in the silk bags to save time',
+  'There are around 3,000 different types of tea.',
+  'Tea has more caffeine than coffee – but it’s not as simple as that.',
+  'You should never use boiling water for tea as you’ll burn the leaf.',
+]
+
+const getRandomFunFact = () => {
+  const randomIndex = Math.floor(Math.random() * funFacts.length)
+  return funFacts[randomIndex]
+}
+
 export function Loader() {
   return (
-    <div className='fixed left-[50%] top-[50%] z-[99] flex h-[200px] w-[500px] -translate-x-1/2 -translate-y-1/2 transform items-center justify-center bg-black opacity-90'>
-      <div className='mx-5 my-5 flex flex-row items-center'>
+    <div className='opacity-99 fixed left-[50%] top-[50%] z-[99] flex h-[200px] w-[500px] -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center bg-black'>
+      <div className='mx-5 my-2 flex flex-row items-center'>
         <img
           src='/brewing.gif'
           alt='Loading...'
@@ -13,6 +37,18 @@ export function Loader() {
           <span className='dot-2'>.</span>
           <span className='dot-3'>.</span>
         </p>
+      </div>
+      <p className='text-xs text-amber-500 text-white'>
+        <b>DID YOU KNOW</b>
+      </p>
+      <div className='mx-16 inline-block h-16 text-center text-xs text-white'>
+        <TypeAnimation
+          sequence={[getRandomFunFact()]}
+          speed={90}
+          repeat={1}
+          wrapper='span'
+          cursor={false}
+        />
       </div>
     </div>
   )
