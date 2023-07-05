3e33366207589a98f101b223e4d1350d81303f74
Rithin Kumar <rithink@amazon.com>
2023-07-05 12:43:24 -0700

    rename elements to trip

--- server/openai/test.js
+++ server/openai/test.js
@@ -1,26 +1,26 @@
 /*
- Simple test example of usage of generateItinerary function
+ Simple test example of usage of generateTrip function
  Run via `node openai/test.js` from `server/` or wherever the .env file with the openai key is
 */
 
-const generateItinerary = require('./generateTrip')
+const generateTrip = require('./generateTrip')
 const TripModel = require('../models/TripModel')
 
-const exampleItinerary = new TripModel({
+const exampleTrip = new TripModel({
   destination: 'Paris',
   budget: 2000,
   numberOfDays: 5,
   stagesPerDay: 3,
 })
 
-;(async function testGenerateItinerary() {
+;(async function testGenerateTrip() {
   try {
-    const itinerary = await generateItinerary(exampleItinerary)
+    const trip = await generateTrip(exampleTrip)
 
-    console.log(`Generated itinerary for given constraints:`)
-    console.log(exampleItinerary)
-    console.log(itinerary)
+    console.log(`Generated trip for given constraints:`)
+    console.log(exampleTrip)
+    console.log(trip)
   } catch (error) {
-    console.error('Error while testing generateItinerary function:', error)
+    console.error('Error while testing generateTrip function:', error)
   }
 })()
