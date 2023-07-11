5781edc6d60ceefec3dfa0bb6eb350157ba8c83a
Graywing13 <30674558+Graywing13@users.noreply.github.com>
2023-07-10 19:04:35 -0700

    Create stages thunks (#146)
    * fix map marker using emoji
    
    * StageModel changes
    
    - remove dayId since we don't have DayModel
    - put latitude first since it's the first number
    - rename colorNumber (remove the u)
    - add locationName and emoji fields
    
    * Add stage endpoints & wire up delete all stages
    
    * Forgot to link up routes
    
    * Fix errors not showing & delete bug
    
    * Add stages thunks
    
    * Fix
    
    ---------
    
    Co-authored-by: Lymeng Naret <lymengnaret@yahoo.com>

---
+++ client/src/redux/reducers/stage/actionTypes.js
@@ -0,0 +1,7 @@
+export const actionTypes = {
+  CREATE_STAGE: 'stages/createStage',
+  GET_STAGE: 'stages/fetchStage',
+  GET_STAGES_BY_TRIP_ID: 'stages/fetchStagesByTripId',
+  UPDATE_STAGE: 'stages/updateStage',
+  DELETE_STAGE: 'stages/deleteStage',
+}
