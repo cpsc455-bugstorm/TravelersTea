b0b62fde972d1f49c90fa675e7b84d4df85d5ad6
Vaishnavi Sinha <59987667+vee-16@users.noreply.github.com>
2023-07-12 22:37:01 -0700

    add getCoordinateTest and update server/README

---
+++ server/googleapi/getCoordinateTest.js
@@ -0,0 +1,29 @@
+/* Run this file with 'yarn coordinates:test' from 'server/' */
+const getStageCoordinates = require('./googleCoordinates')
+
+const stageLocations = [
+  { stageLocationName: 'Eiffel Tower', destination: 'Paris' },
+  { stageLocationName: 'Tim Hortons', destination: 'Vancouver' },
+  { stageLocationName: 'Stanley Park', destination: 'Vancouver' },
+  { stageLocationName: 'Taste of Nawabs', destination: 'Vancouver' },
+]
+
+async function testGenerateTrip() {
+  try {
+    for (const { stageLocationName, destination } of stageLocations) {
+      const coordinates = await getStageCoordinates(
+        stageLocationName,
+        destination,
+      )
+
+      console.log(
+        `----- Coordinates for ${stageLocationName} in ${destination} -----`,
+      )
+      console.log(coordinates)
+    }
+  } catch (error) {
+    console.error('Error while testing generateTrip function:', error)
+  }
+}
+
+testGenerateTrip()
