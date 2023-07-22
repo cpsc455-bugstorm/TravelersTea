9df1991e9c219d3db86e7e1907046a39e1482bd8
Vaishnavi Sinha <59987667+vee-16@users.noreply.github.com>
2023-07-22 12:36:58 -0700

    make globe spin (#198)
    * make globe spin
    
    * Fix compact trip stuck issue

--- client/src/components/MapElement/MapElement.js
+++ client/src/components/MapElement/MapElement.js
@@ -131,6 +131,66 @@ export function MapElement({ className }) {
       })
     })
 
+    // source: https://docs.mapbox.com/mapbox-gl-js/example/globe-spin/
+    // The following values can be changed to control rotation speed:
+    // At low zooms, complete a revolution every two minutes.
+    const secondsPerRevolution = 120
+    // Above zoom level 5, do not rotate.
+    const maxSpinZoom = 5
+    // Rotate at intermediate speeds between zoom levels 3 and 5.
+    const slowSpinZoom = 3
+
+    let userInteracting = false
+    let spinEnabled = true
+
+    function spinGlobe() {
+      const zoom = map.getZoom()
+      if (spinEnabled && !userInteracting && zoom < maxSpinZoom) {
+        let distancePerSecond = 360 / secondsPerRevolution
+        if (zoom > slowSpinZoom) {
+          // Slow spinning at higher zooms
+          const zoomDif = (maxSpinZoom - zoom) / (maxSpinZoom - slowSpinZoom)
+          distancePerSecond *= zoomDif
+        }
+        const center = map.getCenter()
+        center.lng -= distancePerSecond
+        // Smoothly animate the map over one second.
+        // When this animation is complete, it calls a 'moveend' event.
+        map.easeTo({ center, duration: 1000, easing: (n) => n })
+      }
+    }
+
+    // Pause spinning on interaction
+    map.on('mousedown', () => {
+      userInteracting = true
+    })
+
+    // Restart spinning the globe when interaction is complete
+    map.on('mouseup', () => {
+      userInteracting = false
+      spinGlobe()
+    })
+
+    // These events account for cases where the mouse has moved
+    // off the map, so 'mouseup' will not be fired.
+    map.on('dragend', () => {
+      userInteracting = false
+      spinGlobe()
+    })
+    map.on('pitchend', () => {
+      userInteracting = false
+      spinGlobe()
+    })
+    map.on('rotateend', () => {
+      userInteracting = false
+      spinGlobe()
+    })
+
+    map.on('moveend', () => {
+      spinGlobe()
+    })
+    spinGlobe()
+
     setMap(map)
     return () => map.remove()
   }, [])
