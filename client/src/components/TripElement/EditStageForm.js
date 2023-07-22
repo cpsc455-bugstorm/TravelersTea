a4a6428e6010cd940b8c097cd262d60b4f2d8961
Vaishnavi Sinha <59987667+vee-16@users.noreply.github.com>
2023-07-22 11:12:50 -0700

    Adjust plan title dots animation

--- client/src/components/TripElement/EditStageForm.js
+++ client/src/components/TripElement/EditStageForm.js
@@ -36,7 +36,14 @@ export function EditStageForm() {
     <Modal
       open={editStageModalIsOpen}
       handleClose={handleCloseEditStageModal}
-      title='Adjusting Plan...'
+      title={
+        <>
+          Adjusting Plan
+          <span className='dot-1'>.</span>
+          <span className='dot-2'>.</span>
+          <span className='dot-3'>.</span>
+        </>
+      }
     >
       <StageForm onSubmit={onSubmit} />
     </Modal>
