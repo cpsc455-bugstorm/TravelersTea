ea45a3e978a5e7f6264b45f971daf78274cec32c
Lymeng Naret <lymengnaret@yahoo.com>
2023-08-21 20:55:05 -0700

    script to backup db

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const MONGO_USERNAME = process.env.MONGO_USERNAME || ''
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || ''
 const DB_CHOICE = process.env.DB_USER_FLAG || 'BugStormDB'
-const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.4bflh41.mongodb.net/${DB_CHOICE}?retryWrites=true&w=majority`
+const BACKUP_DB_CHOICE = process.env.BACKUP_DB_USER_FLAG || 'BugStormDB'
 
 const SERVER_PORT = process.env.PORT ? Number(process.env.PORT) : 5001
 const ENV = process.env.ENV || ''
@@ -17,7 +17,8 @@ const TIME_LIMT = process.env.TIME_LIMT || 24 * 60 * 60 * 1000 // 1 day
 
 const config = {
   mongo: {
-    uri: MONGO_URI,
+    uri: `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.4bflh41.mongodb.net/${DB_CHOICE}?retryWrites=true&w=majority`,
+    backupuri: `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.4bflh41.mongodb.net/${BACKUP_DB_CHOICE}?retryWrites=true&w=majority`,
     flag: DB_CHOICE,
   },
   server: {
